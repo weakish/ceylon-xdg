dad99716f95c1e71aa98325ea089898f5d4cd18d
Jakukyo Friel <weakish@gmail.com>
2016-11-14 22:30:45 +0800

    Add api doc.

---
+++ api/resources/index.js
@@ -0,0 +1,9 @@
+var index = [
+{'name': 'io.github.weakish.xdg', 'type': 'package', 'url': 'index.html#section-package', 'doc': '<p>Currently implemented the following spec:</p>', 'tags': [],'icons': ['icon-package']},
+{'name': 'XDG.data', 'type': 'value', 'url': 'XDG.type.html#data', 'doc': '<p>$XDG_DATA_HOME/app</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'XDG.config', 'type': 'value', 'url': 'XDG.type.html#config', 'doc': '<p>$XDG_CONFIG_HOMEl/app</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'XDG.cache', 'type': 'value', 'url': 'XDG.type.html#cache', 'doc': '<p>$XDG_CACHE_HOME/app</p>', 'tags': [],'icons': ['icon-shared-member']},
+{'name': 'XDG', 'type': 'class', 'url': 'XDG.type.html', 'doc': '<p>XDG base directories.</p>', 'tags': [],'icons': ['icon-class']},
+{'name': 'io.github.weakish.xdg', 'type': 'module', 'url': 'index.html', 'doc': '<p>XDG base directories for application.</p>', 'tags': [],'icons': ['icon-module']}];
+var tagIndex = [
+];
